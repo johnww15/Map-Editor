096091188dbce6733c61069a14f284dd5d277f7b
Jonathan Q <jonathan.quekww@gmail.com>
2024-06-21 01:14:05 +0800

    created canvas grid

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,10 +1,17 @@
 export default function Canvas({ rows, cols }) {
+  const createGrid = () => {
+    const grid = [];
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        grid.push(<div key={`${i}-${j}`} className="canvas-item"></div>);
+      }
+    }
+    return grid;
+  };
+
   return (
     <>
-      <p>grid</p>
-      <p>
-        {rows}.{cols}
-      </p>
+      <div className="canvas-grid">{createGrid()}</div>
     </>
   );
 }
